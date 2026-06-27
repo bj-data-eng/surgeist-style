4a417a459ec8a6f529a26442d9618c1c148ecb04
bj-data-eng <larks-embassy.5j@icloud.com>
2026-06-27 16:19:18 -0400

    style: adapt retained revision hint

--- src/adapters/retained.rs
+++ src/adapters/retained.rs
@@ -7,7 +7,7 @@ impl<'a> Tree for Snapshot<'a> {
     type Id = Id;
 
     fn version_hint(&self) -> Option<u64> {
-        Some(self.revision())
+        Some(self.revision().get())
     }
 
     fn node(&self, id: Self::Id) -> Result<Node<'_, Self::Id>> {
